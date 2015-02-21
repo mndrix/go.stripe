231ad754cd239df661ed5225535634a565d4128f
Arran Ubels <arran@ubels.com.au>
2015-02-22 10:30:29 +1100

    Subscription was missing an ID.

--- subscription.go
+++ subscription.go
@@ -18,6 +18,7 @@ const (
 //
 // see https://stripe.com/docs/api#subscription_object
 type Subscription struct {
+	Id                 string `json:"id"`
 	Customer           string `json:"customer"`
 	Status             string `json:"status"`
 	Plan               *Plan  `json:"plan"`
